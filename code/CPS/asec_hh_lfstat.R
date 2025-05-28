143451b2b3cc842c0e723e8dab9f932137b6ec00
steviemarvin <marvinstevie01@gmail.com>
2025-05-28 17:11:25 -0400

    re did the base df that the tables pull from

--- code/CPS/asec_hh_lfstat.R
+++ code/CPS/asec_hh_lfstat.R
@@ -1,72 +1,48 @@
-# filter to keep households where the household head has a child
-households_df <- filter(asec_clean,
-                        u18 == 1 & pov200 == 1,
-                        relate == 101) %>% 
-  select(year, serial) %>% 
-  inner_join(asec_clean)
-
+# df of race of household head
 hh_wbhaa <- filter(asec_clean,
                       relate == 101) %>% 
   select(year, serial, wbhaa) %>% 
   rename(wbhaa_hh = wbhaa)
 
-# summarizing labor stat of adults in households
-all_adults <- households_df %>% 
+# baseline df for household labor force stats
+household_lfstat <- asec_clean %>% 
+  # filter to ensure that all households are those where head has a child
+  filter(u18 == 1 & pov200 == 1, relate == 101) %>% 
+  select(year, serial) %>% 
+  # re-join with large df to have all respondents
+  left_join(asec_clean) %>% 
   filter(age >= 18) %>% 
   mutate(
-    # reformatting workly variable
-    workly = case_when(workly == 0 ~ NA,
-                       workly == 1 ~ 0,
-                       workly == 2 ~ 1),
+    # capturing long-term unemployment
+    wksunem_int = case_when(wksunem1 >= 27 & wksunem1 != 99 ~ 1,
+                            wksunem1 < 27 | wksunem1 == 99 ~ 0),
     # variable that indicates adults that are also hh head, spouse or other related indiv.
     related_adults = if_else(age >= 18 &
                                (hh_head_sp == 1 | related_adult == 1 | relate == 1114), 1, 0),
-    related_earner = if_else(earner == 1 & related_adults == 1, 1, 0),
-    # recategorizing variables below to focus just on household head
-    across(offtotval | contains("pov") | workly, 
-           ~if_else(relate == 101, .x, 0)),
-    # earner stat of household head and spouse, variable needed to distinguish between household and family
-    earner_p = case_when(earner == 1 & hh_head_sp == 1 ~ 1,
-                         earner == 0 & hh_head_sp == 0 ~ 0,
-                         TRUE ~ NA),
-    nonearner_p = case_when(nonearner_adult == 1 & hh_head_sp == 1 ~ 1,
-                            nonearner_adult == 0 & hh_head_sp == 1 ~ 0,
-                            TRUE ~ NA)) %>% 
-  # collapsing by household
-  summarize(
-    # max is to get an indicator value from these variables
-    across(offtotval | asecwth | pov100 | pov50 | workly | ft,
-           ~max(.x)),
-    # sum is to get number of each variable for each household
-    across(contains("earner") | related_adults | ft, 
-           ~sum(.x, na.rm = TRUE)),
-    .by = c(year, serial)) %>% 
-  mutate(across(contains("earner") | ft,
-                ~if_else(.x >= 1, 1, 0), 
-                .names = "at1_{.col}"),
-         num_rel_earner = case_when(related_earner == 0 ~ 0,
-                                    related_earner == 1 ~ 1,
-                                    related_earner == 2 ~ 2,
-                                    related_earner >= 3 ~ 3)) %>% 
-  dummy_cols(select_columns = c('num_rel_earner'), ignore_na = TRUE) %>% 
-  left_join(hh_wbhaa)
+    related_ft_earner = if_else(earner == 1 & related_adults == 1 & ft == 1, 1, 0))
   
-samplesize_check <- all_adults %>% 
-  summarize(
-    sum = n(),
-    .by = c(year, wbhaa_hh)) %>% 
-  filter(!is.na(wbhaa_hh)) %>% 
-  pivot_wider(id_cols = year, names_from = wbhaa_hh, values_from = sum)
-
 ### MAIN OUTPUT ###
+# household heads experiencing long term unemployment
+hh_ltunem <- household_lfstat %>% 
+  filter(relate == 101, wkswork1 > 0 & wkswork1 <= 51, !is.na(wbhaa)) %>% 
+  filter(classwly != 0 & classwly != 29 & classwly != 99 & classwly != 13) %>% 
+  summarize(lt_unem = weighted.mean(wksunem_int, w = asecwt, na.rm = TRUE),
+            .by = c(year, wbhaa)) %>% 
+  pivot_wider(id_cols = year, names_from = wbhaa, values_from = lt_unem) %>% 
+  select(year, white, black, hispanic, AIAN, AAPI)
 
-# lf stat summary for households
-hh_lfstat_sum <- all_adults %>% 
-  summarize(
-    at1_ft = weighted.mean(at1_ft, w = asecwth, na.rm = TRUE),
-    num_earner_0 = weighted.mean(num_rel_earner_0, w = asecwth, na.rm = TRUE),
-    .by = c(year, wbhaa_hh)) %>% 
-  filter(!is.na(wbhaa_hh))
+# households with at least 1 full time earner
+at1_earner <- household_lfstat %>% 
+  mutate(asecwth = if_else(relate == 101, asecwth, 0)) %>% 
+  summarize(related_ft_earner = sum(related_ft_earner, na.rm = TRUE),
+            asecwth = max(asecwth),
+            .by = c(year, serial)) %>% 
+  mutate(at1_ft_earner = if_else(related_ft_earner >= 1, 1, 0)) %>% 
+  left_join(hh_wbhaa) %>% 
+  filter(!is.na(wbhaa_hh)) %>% 
+  summarize(at1_ft_earner = weighted.mean(at1_ft_earner, w = asecwth, na.rm = TRUE),
+            .by = c(year, wbhaa_hh)) %>% 
+  pivot_wider(id_cols = year, names_from = wbhaa_hh, values_from = at1_ft_earner) %>% 
+  select(year, white, black, hispanic, AIAN, AAPI)
+  
 
-at1_earner <- hh_lfstat_sum %>% 
-  pivot_wider(id_cols = year, names_from = wbhaa_hh, values_from = at1_ft)
--- code/CPS/asec_poverty.R
+++ code/CPS/asec_poverty.R
@@ -1,18 +1,21 @@
 #Poverty benchmarking and charts
+poverty <- asec_clean %>% 
+  filter(u18 == 1 & pov200 == 1 & relate == 101)
+
 # share below FPL and below 50% of FPL
-povertylevels <- all_adults %>% 
+povertylevels <- poverty %>% 
   summarize(across(pov100 | pov50, ~weighted.mean(.x, w = asecwth, na.rm = TRUE),
                    .names = "share_{.col}"),
             .by = year)
 
 # share below FPL and below 50% of FPL by race and ethnicity
-povertylevelsXwbhaa <- all_adults %>% 
+povertylevelsXwbhaa <- poverty %>% 
   summarize(pov100 = weighted.mean(pov100, w = asecwth, na.rm = TRUE),
             pov50 = weighted.mean(pov50, w = asecwth, na.rm = TRUE),
-            .by = c(year, wbhaa_hh)) %>% 
-  filter(!is.na(wbhaa_hh)) %>% 
+            .by = c(year, wbhaa)) %>% 
+  filter(!is.na(wbhaa)) %>% 
   pivot_wider(id_cols = year, 
-              names_from = wbhaa_hh, 
+              names_from = wbhaa, 
               values_from = c("pov100", "pov50"))
 
 # function that reformats df povertylevelsXwbhaa for exporting
@@ -28,24 +31,25 @@ povlev_reformat_fun <- function(data, povertylevel){
 povertylevelsXwbhaa100 <- povlev_reformat_fun(povertylevelsXwbhaa, "pov100")
 povertylevelsXwbhaa50 <- povlev_reformat_fun(povertylevelsXwbhaa, "pov50")
 
+
 povertyXwbhaa_fun <- function(data, povertylevel){
   data %>%
     filter({{povertylevel}} == 1) %>% 
-    dummy_cols(select_columns = c('wbhaa_hh'), ignore_na = TRUE) %>% 
-    summarize(across(contains("wbhaa_hh_"), ~weighted.mean(.x, w = asecwth, na.rm = TRUE)),
+    dummy_cols(select_columns = c('wbhaa'), ignore_na = TRUE) %>% 
+    summarize(across(contains("wbhaa_"), ~weighted.mean(.x, w = asecwth, na.rm = TRUE)),
               .by = year) %>% 
-    rename_with(~gsub("wbhaa_hh_", "", .), contains("wbhaa_hh_")) %>% 
+    rename_with(~gsub("wbhaa_", "", .), contains("wbhaa_")) %>% 
     select(year, white, black, hispanic, AIAN, AAPI)
 }
 
 
-pov200Xwbhaa <- all_adults %>% 
-  dummy_cols(select_columns = c('wbhaa_hh'), ignore_na = TRUE) %>% 
-  summarize(across(contains("wbhaa_hh_"), ~weighted.mean(.x, w = asecwth, na.rm = TRUE)),
+pov200Xwbhaa <- poverty %>% 
+  dummy_cols(select_columns = c('wbhaa'), ignore_na = TRUE) %>% 
+  summarize(across(contains("wbhaa_"), ~weighted.mean(.x, w = asecwth, na.rm = TRUE)),
             .by = year) %>% 
-  rename_with(~gsub("wbhaa_hh_", "", .), contains("wbhaa_hh_"))%>% 
+  rename_with(~gsub("wbhaa_", "", .), contains("wbhaa_"))%>% 
   select(year, white, black, hispanic, AIAN, AAPI)
 
-pov100Xwbhaa <- povertyXwbhaa_fun(all_adults, pov100)
+pov100Xwbhaa <- povertyXwbhaa_fun(poverty, pov100)
 
-pov50Xwbhaa <- povertyXwbhaa_fun(all_adults, pov50)
+pov50Xwbhaa <- povertyXwbhaa_fun(poverty, pov50)
